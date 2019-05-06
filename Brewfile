c70165f7cd1e5d1644d74f15fd13e4e9161f1f07
Adrian Booth <Adrian.booth17@gmail.com>
2019-05-06 21:55:10 +0100

    add Brewfile and bootstrap script for automated installation on new machines

---
+++ Brewfile
@@ -0,0 +1,28 @@
+tap 'thoughtbot/formulae'
+tap 'caskroom/cask'
+
+brew 'vim'
+brew 'git'
+brew 'python'
+brew 'the_silver_searcher'
+brew 'tmux'
+brew 'mysql'
+brew 'go'
+
+brew 'rbenv', args: ['HEAD']
+brew 'ruby-build', args: ['HEAD']
+
+brew 'node'
+brew 'postgresql'
+brew 'redis'
+
+cask 'slack'
+cask 'docker'
+cask 'flux'
+cask 'atom'
+cask 'spotify'
+cask 'iterm2'
+cask 'skype'
+cask 'google-chrome'
+cask 'firefox'
+cask 'postgres'
---
+++ bootstrap.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+echo 'Installing a new machine? Nice!'
+if ! command -v brew > /dev/null 2>&1; then
+  echo 'Okay, lets do this. First things first, we need to install homebrew in order to get all the stuff you need'
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  echo 'Homebrew was already available... skipping the installation'
+  echo "Let's update it instead! (you know you haven't updated in a while)"
+  brew update
+fi
+
+# HERE WE WILL START WITH THE ACTUALL INSTALLATION
+
+echo 'Brewing...'
+brew tap Homebrew/bundle
+brew bundle
+echo "Brew complete"
+
+echo 'Installing Ruby...'
+rbenv install 2.5.0
+rbenv global 2.5.0
+
+echo "Ruby installed"
+
+for file in $( ls -A | grep -vE '\.exclude*|\.git$|\.gitignore|.*.md' ) ; do
+      ln -sv "$PWD/$file" "$HOME"
+done
+# TODO: source files here?
+echo "Symlinking complete"
